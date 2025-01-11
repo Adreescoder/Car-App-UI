73120f3f246c818912953043874121b83f54ee8e
Adrees White Clean  Coder <adreesking975@gmail.com>
2025-01-11 22:47:45 +0500

    working start

---
+++ car_app/lib/screen/home__screen/view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'logic.dart';
+
+class Home_ScreenPage extends StatelessWidget {
+  Home_ScreenPage({Key? key}) : super(key: key);
+
+  final Home_ScreenLogic logic = Get.put(Home_ScreenLogic());
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
