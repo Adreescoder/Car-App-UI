acb47e02406ef33cddb1e964813722567bfde6b4
Adrees White Clean  Coder <adreesking975@gmail.com>
2025-01-12 14:18:07 +0500

    chage file code

--- car_app/lib/main.dart
+++ car_app/lib/main.dart
@@ -1,21 +1,32 @@
-import 'package:car_app/screen/home__screen/view.dart';
+import 'package:car_app/services/firebase.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const RabbitCoinApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class RabbitCoinApp extends StatelessWidget {
+  const RabbitCoinApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'mini app',
-      home: Home_ScreenPage(),
+      title: 'Rabbit Coin',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.black,
+        brightness: Brightness.dark,
+      ),
+      home: RootScreen(),
     );
   }
 }
